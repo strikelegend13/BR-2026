047294eb186571093d85a0f7173b6342ef1d958e
strikelegend13 <bevanroy133@gmail.com>
2026-02-23 23:26:43 -0500

    Update webserver.py

--- networking&data/webserver.py
+++ networking&data/webserver.py
@@ -1,4 +1,4 @@
-# simple web server for yourself
+# simple web server for yourself, crafted through university provided skeleton code
 # import socket module
 from socket import *
 # In order to terminate the program
@@ -21,22 +21,15 @@ def webServer(port=13331):
       header = b"HTTP/1.1 200 OK\r\nServer: helloworld.html Server\r\n"
       outputdata = header + b"Content-Type: text/html; charset=UTF-8\r\n"
       content = b""
-      #Content-Type is an example on how to send a header as bytes. There are more!
-      #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-      #Fill in end
       for i in f: #for line in file
         content += i
       f.close()
-      #Send the content of the requested file to the client (don't forget the headers you created)!
-      #Send everything as one send command, do not send one line/item at a time
       response = outputdata + f"Content-Length: {len(content)}\r\n".encode() + b"Connection: close\r\n\r\n" + content
       connectionSocket.sendall(response)
         
       connectionSocket.close() #closing the connection socket
       
     except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      # Remember the format you used in the try: block!
       error = b"<html><body><h1>404 Not Found</h1></body></html>"
       errorback = (
         b"HTTP/1.1 404 Not Found\r\n"
@@ -49,14 +42,13 @@ def webServer(port=13331):
       connectionSocket.sendall(errorback)
 
 
-      #Close client socket
       connectionSocket.close()
-  # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
-  # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
+
   #serverSocket.close()
   #sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
 
   webServer(13331)
+
 
